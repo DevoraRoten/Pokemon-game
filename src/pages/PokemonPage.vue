fae42bb9edf1556891e0737fa3c9f040d3c6b8a0
Devora Rohten <d.rohten01@gamil.com>
2023-03-13 13:04:34 -0300

    formato pokemons game

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>¿Quién es este Pokemon?</h1>
+  <PokemonPicture></PokemonPicture>
+  <pokemon-options></pokemon-options>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture';
+import PokemonOptions from '@/components/PokemonOptions';
+
+export default {
+  name: 'PokemonPage',
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
